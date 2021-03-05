1230aa5134b69c2903c7b2339f222500b27e12da
Amin Kharaghani <amin.kharaghani@mail.utoronto.ca>
2021-03-05 14:03:00 -0500

    Updated dip test P-value threshhold (unadjusted), and SNP count inclusion.

--- Analysis/Creating_main_dataframes_for_EDA.R
+++ Analysis/Creating_main_dataframes_for_EDA.R
@@ -21,12 +21,15 @@ geno_pcs <- read.table("/Users/amink/OneDrive/Documents/Current Jobs/Masters The
 names(geno_pcs) <- c("FID","IID",paste0("genoPC",seq(1:10)))
 
 
-# -------------------------------------# Here we can alternate between the PRS with no MHC and with MHC # -------------------------------------------------#                                                                                       #|
-                                                                                                                                                           #|                                                                                                     
-prs_filenames <- list.files("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/PRS_PRSice/",full.names = T) ; no_mhc = FALSE      #|
+# -------------------------------------# Here we can alternate between the PRS with no MHC and with MHC # ------------------------------------------------- #                                                                                       #|
+                                                                                                                                                            #|                                                                                                     
+#prs_filenames <- list.files("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/PRS_PRSice/",full.names = T) ; no_mhc = FALSE        #|
 #prs_filenames <- list.files("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/PRS_PRSice_No_MHC/",full.names = T) ; no_mhc = TRUE #|
-                                                                                                                                                           #|
-#----------------------------------------------------------------------------------------------------------------------------------------------------------#
+                                                                                                                                                            #|
+#prs_filenames <- list.files("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/CLUMP_200_0.25/PRS_PLINK/",full.names = T) ; no_mhc = FALSE        #|
+prs_filenames <- list.files("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/CLUMP_200_0.25/PRS_PLINK_No_MHC/",full.names = T) ; no_mhc = TRUE #|
+                                                                                                                                                            #|
+#---------------------------------------------------------------------------------------------------------------------------------------------------------- #
 
 
 # read in PRS files
@@ -36,22 +39,34 @@ for (prsfile in prs_filenames[1:10]) {
 }
 
 # read the SNP count file
-snp_count <- as.data.frame(fread('/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/PRS_PRSice/SNP_count/snp_count_cumulative.txt',header=T))
-
-snp_count$category <- c("p_val_1.txt","p_val_0_1.txt","p_val_0_05.txt","p_val_0_01.txt",
-                        "p_val_0_001.txt","p_val_0_0001.txt","p_val_1e-05.txt","p_val_1e-06.txt", 
-                        "p_val_1e-07.txt","p_val_5e-08.txt")               
+# snp_count <- as.data.frame(fread('/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/PRS_PRSice/SNP_count/snp_count_cumulative.txt',header=T))
+# 
+# snp_count$category <- c("p_val_1.txt","p_val_0_1.txt","p_val_0_05.txt","p_val_0_01.txt",
+#                         "p_val_0_001.txt","p_val_0_0001.txt","p_val_1e-05.txt","p_val_1e-06.txt", 
+#                         "p_val_1e-07.txt","p_val_5e-08.txt")               
                 
 ## LOOP for PCA analysis
 dsets_torun <- names(prs_list)
 
 allres <- list()
+
+for (thresh in dsets_torun) {
+                print(thresh)
+                print('FID'%in%colnames(prs_list[[thresh]]))
+                print('IID'%in%colnames(prs_list[[thresh]]))
+}
+
+# The FID name for "p_val_0_001.txt" (PRSs with MHC with PLINK) is ataFID. Let's change that:
+thresh <- "p_val_0_001.txt"
+colnames(prs_list[[thresh]])[1] <- 'FID'
+
+# No changes needed for non_mhc PRS (PLINK2.0)
+
 for (thresh in dsets_torun) {
                 pval <- gsub(".txt","",thresh)
                 print(paste("Starting run, p-value threshold=",pval))
                 print(paste("number of subjects =",nrow(prs_list[[thresh]])))
                 print(paste("number of PRS =",ncol(prs_list[[thresh]])-2))
-                
                 formod <- merge(prs_list[[thresh]],geno_pcs,by=c("IID","FID"))
                 
                 # this could be removed from loop, but in case any subjects are removed/added to either geno_pcs or PRS files, the design matrix will need to be recalculated, so left in to prevent error
@@ -65,24 +80,20 @@ for (thresh in dsets_torun) {
                 
                 print(paste("Extracting model residuals"))
                 res <- t(residuals.MArrayLM(fit,y = t(formod[,3:ncol(prs_list[[thresh]])])))
-                #res <- apply(res,2,scale) 
+                res <- apply(res,2,scale) 
                 
-                res <- scale(res,scale=FALSE) # Change this later maybe
-                # # SNP count flag:
-                # print(paste("Number of phenotypes with more than 100 SNPs used in their PRS is",
-                #             (sum(snp_count[which(snp_count$category == thresh),] > 99)-1))) # removing 1 : category column
-                # pheno_passed <- names(snp_count[,which(snp_count[which(snp_count$category == thresh),
-                #                                                  ] > 99)])[-sum(snp_count[which(snp_count$category == thresh),
-                #                                                                           ] > 99)]
-                # 
-                # res <- as.matrix(as.data.frame(res)[,which(names(as.data.frame(res)) %in% pheno_passed)])
+                #res <- scale(res,scale=TRUE) # Change this later maybe
                 
-                # flag multimodal scores
+                # Finding columns with NAs
+                na_cols <- colnames(res)[ apply(res, 2, anyNA) ]
+                print(paste0('number of columns with NAs ',length(na_cols)))
+                res <- res[,which(!colnames(res) %in% na_cols)]
+                
+                # flag multimodal scores - Using dip test
                 print(paste("Flagging scores with multimodal distributions"))
                 dts <- apply(res,2,dip.test)
-                
                 dtsp <- lapply(dts,function(x) { x$p.value })
-                multimodal <- which(dtsp < 0.05/ncol(res))
+                multimodal <- which(dtsp < 0.05) #/ncol(res)
                 
                 print(paste("Removing",length(multimodal),"multimodal scores"))
                 
@@ -94,6 +105,16 @@ for (thresh in dsets_torun) {
                                 res.clean <- res
                 }
                 
+
+                # # SNP count flag:
+                # print(paste("Number of phenotypes with more than 100 SNPs used in their PRS is",
+                #             (sum(snp_count[which(snp_count$category == thresh),] > 99)-1))) # removing 1 : category column
+                # pheno_passed <- names(snp_count[,which(snp_count[which(snp_count$category == thresh),
+                #                                                  ] > 99)])[-sum(snp_count[which(snp_count$category == thresh),
+                #                                                                           ] > 99)]
+                # 
+                # res.clean <- as.matrix(as.data.frame(res.clean)[,which(names(as.data.frame(res.clean)) %in% pheno_passed)])
+                
                 # Change the phenotype labels
                 
                 colnames(res.clean) <- meta_pheno[which(meta_pheno$phenocode_annotate_lst %in% colnames(res.clean)),]$new_pheno_annot
@@ -103,40 +124,31 @@ for (thresh in dsets_torun) {
                 matrix <- as.data.frame(res.clean[,-dim(res.clean)[2]])
                 matrix_dist <- as.matrix(dist(t(matrix)))
                 dist_df <- melt_dist(matrix_dist)
-                # #dist_df_passed <- dist_df[which(log(dist_df$dist) >= quantile(log(dist_df$dist),0.025)),]
-                # check <- dist_df[which(log(dist_df$dist) < quantile(log(dist_df$dist),0.025)),]
-                # to_remove = NULL
-                # for (i in 1:dim(check)[1]){
-                #                 corr <- cor.test(as.numeric(unlist(matrix[check[i,1]])),
-                #                                  as.numeric(unlist(matrix[check[i,2]])))
-                #                 if((corr$p.value <0.05/dim(matrix[2])) & (abs(corr$estimate)>0.95)){
-                #                                 if (grepl( "IC", matrix[check[i,1]], fixed = TRUE)){
-                #                                                 to_remove <- append(to_remove,check[i,2])
-                #                                 } else if (grepl( "IC", matrix[check[i,2]], fixed = TRUE)){
-                #                                                 to_remove <- append(to_remove,check[i,1])
-                #                                 } else if(as.numeric(unlist(strsplit(unlist(strsplit(check[i,1],'_'))[length(unlist(strsplit(check[i,1],'_')))],'n'))[2]) > 
-                #                                           as.numeric(unlist(strsplit(unlist(strsplit(check[i,2],'_'))[length(unlist(strsplit(check[i,2],'_')))],'n'))[2])){
-                #                                                 to_remove <- append(to_remove,check[i,2])
-                #                                 } else if (as.numeric(unlist(strsplit(unlist(strsplit(check[i,1],'_'))[length(unlist(strsplit(check[i,1],'_')))],'n'))[2]) < 
-                #                                            as.numeric(unlist(strsplit(unlist(strsplit(check[i,2],'_'))[length(unlist(strsplit(check[i,2],'_')))],'n'))[2])){
-                #                                                 to_remove <- append(to_remove,check[i,1])
-                #                                 } else if (as.numeric(unlist(strsplit(unlist(strsplit(check[i,1],'_'))[(length(unlist(strsplit(check[i,1],'_')))-1)],'h'))[2]) < 
-                #                                            as.numeric(unlist(strsplit(unlist(strsplit(check[i,2],'_'))[(length(unlist(strsplit(check[i,2],'_')))-1)],'h'))[2])){
-                #                                                 to_remove <- append(to_remove,check[i,1])
-                #                                 } else if(as.numeric(unlist(strsplit(unlist(strsplit(check[i,1],'_'))[(length(unlist(strsplit(check[i,1],'_')))-1)],'h'))[2]) > 
-                #                                           as.numeric(unlist(strsplit(unlist(strsplit(check[i,2],'_'))[(length(unlist(strsplit(check[i,2],'_')))-1)],'h'))[2])){
-                #                                                 to_remove <- append(to_remove,check[i,2])
-                #                                 } else {
-                #                                                 to_remove <- append(to_remove,sample(c(check[i,1], check[i,2]), 1))
-                #                                 }
-                #                 }
-                # }
-                # 
-                # print(paste0(length(to_remove)," duplicated phenotypes were found"))
-                
-                #res.clean <- as.matrix(as.data.frame(res.clean)[,which(!colnames(res.clean)%in%to_remove)])
-                
-                
+                #dist_df_passed <- dist_df[which(log(dist_df$dist) >= quantile(log(dist_df$dist),0.025)),]
+                check <- dist_df[which(log(dist_df$dist) < quantile(log(dist_df$dist),0.025)),]
+                to_remove = NULL
+                for (i in 1:dim(check)[1]){
+                                corr <- cor.test(as.numeric(unlist(matrix[check[i,1]])),
+                                                 as.numeric(unlist(matrix[check[i,2]])))
+                                if((corr$p.value <0.05/dim(matrix[2])) & (abs(corr$estimate)>0.95)){
+                                                if (grepl( "IC", matrix[check[i,1]], fixed = TRUE)){
+                                                                to_remove <- append(to_remove,check[i,2])
+                                                } else if(grepl( "IC", matrix[check[i,2]], fixed = TRUE)){
+                                                                to_remove <- append(to_remove,check[i,1])
+                                                } else if (grepl( "PH", matrix[check[i,1]], fixed = TRUE)){
+                                                                to_remove <- append(to_remove,check[i,2])
+                                                } else if (grepl( "PH", matrix[check[i,2]], fixed = TRUE)){
+                                                                to_remove <- append(to_remove,check[i,1])
+                                                } else{
+                                                                to_remove <- append(to_remove,sample(c(check[i,1], check[i,2]), 1))
+                                                }
+                                }
+                }
+
+                print(paste0(length(to_remove)," duplicated phenotypes were found"))
+
+                res.clean <- as.matrix(as.data.frame(res.clean)[,which(!colnames(res.clean)%in%to_remove)])
+
                 # run PCA
                 print(paste("Running PCA"))
                 respca <- prcomp(res.clean)
@@ -159,9 +171,18 @@ for (thresh in dsets_torun) {
                                res.var=res.var,
                                res.ind=res.ind)
                 if(isTRUE(no_mhc)){
-                                saveRDS(allres,file=paste0("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/PRS_PRSice_No_MHC_Resid_PCA_Clust/Scaled/", "PCAresults_",pval,".rds"))
+                                
+                                #PRSice#
+                                #saveRDS(allres,file=paste0("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/PRS_PRSice_No_MHC_Resid_PCA_Clust/Scaled/", "PCAresults_",pval,".rds"))
+                                
+                                #PLINK#
+                                saveRDS(allres,file=paste0("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/CLUMP_200_0.25/PRS_PLINK_No_MHC_Resid_PCA_Clust/", "PCAresults_",pval,".rds"))
                 } else{
-                                saveRDS(allres,file=paste0("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/PRS_PRSice_Resid_PCA_Clust/Scaled/", "PCAresults_",pval,".rds"))         
+                                #PRSice#
+                                #saveRDS(allres,file=paste0("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/PRS_PRSice_Resid_PCA_Clust/Scaled/", "PCAresults_",pval,".rds"))
+                                
+                                #PLINK#
+                                saveRDS(allres,file=paste0("/Users/amink/OneDrive/Documents/Current Jobs/Masters Thesis/Code/Datasets/CLUMP_200_0.25/PRS_PLINK_Resid_PCA_Clust/", "PCAresults_",pval,".rds"))  
                 }
 }
 
